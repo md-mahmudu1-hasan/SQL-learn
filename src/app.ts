e44588cd889b6898488c21bea9ca46ac5d51b168
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2026-06-18 14:09:38 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {
+import  express, {
   type Application,
   type Request,
   type Response,
@@ -6,13 +6,15 @@ import express, {
 import { userRouter } from "./modules/users/users.routes";
 import { profileRouter } from "./modules/profiles/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
-import cookieParser from "cookie-parser";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
 
 const app: Application = express();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
